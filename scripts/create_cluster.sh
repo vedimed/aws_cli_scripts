528c5722304779d923f021da7aae5f6284ca5933
sandeepkp1175 <sandeep.pattnaik@gmail.com>
2021-02-24 20:36:54 -0600

    Update create_cluster.sh

--- scripts/create_cluster.sh
+++ scripts/create_cluster.sh
@@ -1,7 +1,7 @@
 eksctl create cluster \
---name vedimed-dev \
+--name vedimed-m5-dev \
 --version 1.17 \
 --region us-east-1 \
---nodegroup-name vedimed-dev-node-group \
---node-type t2.micro \
+--nodegroup-name vedimed-m5-dev-node-group \
+--node-type m5.xlarge \
 --nodes 2
